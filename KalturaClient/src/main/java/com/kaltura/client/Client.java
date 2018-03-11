4a34c9e01a822804d1026a81b9240bc88e20071e
Jenkins <jenkins@kaltura.com>
2018-03-11 09:47:57 +0000

    Auto-generated by Jenkins

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2017  Kaltura Inc.
+// Copyright (C) 2006-2018  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -43,8 +43,8 @@ public class Client extends ClientBase {
 	public Client(ConnectionConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:18-03-10");
-		this.setApiVersion("4.72.12.43158");
+		this.setClientTag("java:18-03-11");
+		this.setApiVersion("4.72.12.17590");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
