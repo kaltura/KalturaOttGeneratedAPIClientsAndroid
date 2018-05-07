3baa5e59aa2cff9f50b623a115436a9c6e414769
Jenkins <jenkins@kaltura.com>
2018-05-07 06:50:17 +0000

    Auto-generated by Jenkins

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -44,7 +44,7 @@ public class Client extends ClientBase {
 		super(config);
 		
 		this.setClientTag("java:18-05-07");
-		this.setApiVersion("4.81.28.10968");
+		this.setApiVersion("4.81.28.12289");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
