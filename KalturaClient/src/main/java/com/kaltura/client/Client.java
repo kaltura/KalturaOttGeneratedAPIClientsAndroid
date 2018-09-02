568656b62aed6505b8a14f2e07677864134524db
Jenkins <jenkins@kaltura.com>
2018-09-02 14:15:30 +0000

    Auto-generated by Jenkins

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -44,7 +44,7 @@ public class Client extends ClientBase {
 		super(config);
 		
 		this.setClientTag("java:18-09-02");
-		this.setApiVersion("5.0.2.23784");
+		this.setApiVersion("5.0.2.25605");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
--- KalturaClient/src/main/java/com/kaltura/client/types/BaseSearchAssetFilter.java
+++ KalturaClient/src/main/java/com/kaltura/client/types/BaseSearchAssetFilter.java
@@ -76,7 +76,7 @@ public abstract class BaseSearchAssetFilter extends AssetFilter {
 	  (not), ~ (like), !~, ^ (any word starts with), ^= (phrase starts with), +
 	  (exists), !+ (not exists).              Logical conjunction: and, or.           
 	     Search values are limited to 20 characters each for the next operators: ~,
-	  !~, ^, ^=              (maximum length of entire filter is 2048 characters)
+	  !~, ^, ^=              (maximum length of entire filter is 4096 characters)
 	 */
 	private String kSql;
 	/**
