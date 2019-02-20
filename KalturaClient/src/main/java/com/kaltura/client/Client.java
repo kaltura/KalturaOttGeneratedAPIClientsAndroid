3224e9a01ea5a23574a43832040a8089968fedbc
Backend CI <ott.rnd.core@kaltura.com>
2019-02-20 14:16:04 +0000

    Auto-generated by Jenkins

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -44,8 +44,8 @@ public class Client extends ClientBase {
 	public Client(ConnectionConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:19-02-19");
-		this.setApiVersion("5.1.1.42123");
+		this.setClientTag("java:19-02-20");
+		this.setApiVersion("5.1.1.25422");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
--- KalturaClient/src/main/java/com/kaltura/client/types/PlaybackContextOptions.java
+++ KalturaClient/src/main/java/com/kaltura/client/types/PlaybackContextOptions.java
@@ -60,7 +60,7 @@ public class PlaybackContextOptions extends ObjectBase {
 	 */
 	private String mediaProtocol;
 	/**
-	 * Playback streamer type: applehttp, mpegdash, url.
+	 * Playback streamer type: applehttp, mpegdash, url, smothstreaming, none
 	 */
 	private String streamerType;
 	/**
