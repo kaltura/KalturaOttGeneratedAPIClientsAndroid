d4d6c3c482363052b5e0eb972e93aab4fbac268d
Backend CI <ott.rnd.core@kaltura.com>
2019-09-11 07:51:29 +0000

    Auto-generated by Jenkins

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -45,7 +45,7 @@ public class Client extends ClientBase {
 		super(config);
 		
 		this.setClientTag("java:19-09-11");
-		this.setApiVersion("5.2.6.5948");
+		this.setApiVersion("5.2.6.5958");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
--- KalturaClient/src/main/java/com/kaltura/client/types/Region.java
+++ KalturaClient/src/main/java/com/kaltura/client/types/Region.java
@@ -54,6 +54,7 @@ public class Region extends ObjectBase {
 		String externalId();
 		String isDefault();
 		RequestBuilder.ListTokenizer<RegionalChannel.Tokenizer> linearChannels();
+		String parentId();
 	}
 
 	/**
@@ -76,6 +77,10 @@ public class Region extends ObjectBase {
 	 * List of associated linear channels
 	 */
 	private List<RegionalChannel> linearChannels;
+	/**
+	 * Parent region ID
+	 */
+	private Long parentId;
 
 	// id:
 	public Integer getId(){
@@ -117,20 +122,24 @@ public class Region extends ObjectBase {
 	public Boolean getIsDefault(){
 		return this.isDefault;
 	}
-	public void setIsDefault(Boolean isDefault){
-		this.isDefault = isDefault;
-	}
-
-	public void isDefault(String multirequestToken){
-		setToken("isDefault", multirequestToken);
-	}
-
 	// linearChannels:
 	public List<RegionalChannel> getLinearChannels(){
 		return this.linearChannels;
 	}
 	public void setLinearChannels(List<RegionalChannel> linearChannels){
 		this.linearChannels = linearChannels;
+	}
+
+	// parentId:
+	public Long getParentId(){
+		return this.parentId;
+	}
+	public void setParentId(Long parentId){
+		this.parentId = parentId;
+	}
+
+	public void parentId(String multirequestToken){
+		setToken("parentId", multirequestToken);
 	}
 
 
@@ -149,6 +158,7 @@ public class Region extends ObjectBase {
 		externalId = GsonParser.parseString(jsonObject.get("externalId"));
 		isDefault = GsonParser.parseBoolean(jsonObject.get("isDefault"));
 		linearChannels = GsonParser.parseArray(jsonObject.getAsJsonArray("linearChannels"), RegionalChannel.class);
+		parentId = GsonParser.parseLong(jsonObject.get("parentId"));
 
 	}
 
@@ -158,8 +168,8 @@ public class Region extends ObjectBase {
 		kparams.add("id", this.id);
 		kparams.add("name", this.name);
 		kparams.add("externalId", this.externalId);
-		kparams.add("isDefault", this.isDefault);
 		kparams.add("linearChannels", this.linearChannels);
+		kparams.add("parentId", this.parentId);
 		return kparams;
 	}
 
@@ -189,6 +199,7 @@ public class Region extends ObjectBase {
         } else {
             dest.writeInt(-1);
         }
+        dest.writeValue(this.parentId);
     }
 
     public Region(Parcel in) {
@@ -202,6 +213,7 @@ public class Region extends ObjectBase {
             this.linearChannels = new ArrayList<>();
             in.readList(this.linearChannels, RegionalChannel.class.getClassLoader());
         }
+        this.parentId = (Long)in.readValue(Long.class.getClassLoader());
     }
 }
 
