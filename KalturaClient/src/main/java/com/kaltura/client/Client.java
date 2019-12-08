7addec84a86511c6cc8120249297b20a460e138d
Backend CI <ott.rnd.core@kaltura.com>
2019-12-08 06:06:03 +0000

    Auto-generated by Jenkins

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -44,8 +44,8 @@ public class Client extends ClientBase {
 	public Client(ConnectionConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:19-12-07");
-		this.setApiVersion("5.3.0.14227");
+		this.setClientTag("java:19-12-08");
+		this.setApiVersion("5.3.0.14228");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
