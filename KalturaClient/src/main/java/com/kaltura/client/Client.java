0d7a719142e32eddb8e6c177522cfebd54748f21
Backend CI <ott.rnd.core@kaltura.com>
2020-03-04 21:31:12 +0000

    Auto-generated by Jenkins job OTT-BE-Phoenix-Windows, build 2778, branch master

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -45,7 +45,7 @@ public class Client extends ClientBase {
 		super(config);
 		
 		this.setClientTag("java:20-03-04");
-		this.setApiVersion("5.3.2.14741");
+		this.setApiVersion("5.3.2.14743");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
