881c2b6c4ab08929acb74431c9e220de57bad08f
Backend CI <ott.rnd.core@kaltura.com>
2020-11-16 01:24:23 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/168, branch 5_8_1

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/EntitlementRenewal.java
@@ -0,0 +1,196 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.types.ObjectBase;
+import com.kaltura.client.types.Price;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(EntitlementRenewal.Tokenizer.class)
+public class EntitlementRenewal extends ObjectBase {
+	
+	public interface Tokenizer extends ObjectBase.Tokenizer {
+		Price.Tokenizer price();
+		String date();
+		String purchaseId();
+		String subscriptionId();
+		String userId();
+	}
+
+	/**
+	 * Price that is going to be paid on the renewal
+	 */
+	private Price price;
+	/**
+	 * Next renewal date
+	 */
+	private Long date;
+	/**
+	 * Puchase ID
+	 */
+	private Long purchaseId;
+	/**
+	 * Subscription ID
+	 */
+	private Long subscriptionId;
+	/**
+	 * User ID
+	 */
+	private Long userId;
+
+	// price:
+	public Price getPrice(){
+		return this.price;
+	}
+	public void setPrice(Price price){
+		this.price = price;
+	}
+
+	// date:
+	public Long getDate(){
+		return this.date;
+	}
+	public void setDate(Long date){
+		this.date = date;
+	}
+
+	public void date(String multirequestToken){
+		setToken("date", multirequestToken);
+	}
+
+	// purchaseId:
+	public Long getPurchaseId(){
+		return this.purchaseId;
+	}
+	public void setPurchaseId(Long purchaseId){
+		this.purchaseId = purchaseId;
+	}
+
+	public void purchaseId(String multirequestToken){
+		setToken("purchaseId", multirequestToken);
+	}
+
+	// subscriptionId:
+	public Long getSubscriptionId(){
+		return this.subscriptionId;
+	}
+	public void setSubscriptionId(Long subscriptionId){
+		this.subscriptionId = subscriptionId;
+	}
+
+	public void subscriptionId(String multirequestToken){
+		setToken("subscriptionId", multirequestToken);
+	}
+
+	// userId:
+	public Long getUserId(){
+		return this.userId;
+	}
+	public void setUserId(Long userId){
+		this.userId = userId;
+	}
+
+	public void userId(String multirequestToken){
+		setToken("userId", multirequestToken);
+	}
+
+
+	public EntitlementRenewal() {
+		super();
+	}
+
+	public EntitlementRenewal(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		price = GsonParser.parseObject(jsonObject.getAsJsonObject("price"), Price.class);
+		date = GsonParser.parseLong(jsonObject.get("date"));
+		purchaseId = GsonParser.parseLong(jsonObject.get("purchaseId"));
+		subscriptionId = GsonParser.parseLong(jsonObject.get("subscriptionId"));
+		userId = GsonParser.parseLong(jsonObject.get("userId"));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaEntitlementRenewal");
+		kparams.add("price", this.price);
+		kparams.add("date", this.date);
+		kparams.add("purchaseId", this.purchaseId);
+		kparams.add("subscriptionId", this.subscriptionId);
+		kparams.add("userId", this.userId);
+		return kparams;
+	}
+
+
+    public static final Creator<EntitlementRenewal> CREATOR = new Creator<EntitlementRenewal>() {
+        @Override
+        public EntitlementRenewal createFromParcel(Parcel source) {
+            return new EntitlementRenewal(source);
+        }
+
+        @Override
+        public EntitlementRenewal[] newArray(int size) {
+            return new EntitlementRenewal[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeParcelable(this.price, flags);
+        dest.writeValue(this.date);
+        dest.writeValue(this.purchaseId);
+        dest.writeValue(this.subscriptionId);
+        dest.writeValue(this.userId);
+    }
+
+    public EntitlementRenewal(Parcel in) {
+        super(in);
+        this.price = in.readParcelable(Price.class.getClassLoader());
+        this.date = (Long)in.readValue(Long.class.getClassLoader());
+        this.purchaseId = (Long)in.readValue(Long.class.getClassLoader());
+        this.subscriptionId = (Long)in.readValue(Long.class.getClassLoader());
+        this.userId = (Long)in.readValue(Long.class.getClassLoader());
+    }
+}
+
---
+++ KalturaClient/src/main/java/com/kaltura/client/types/PurchaseSession.java
@@ -0,0 +1,114 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(PurchaseSession.Tokenizer.class)
+public class PurchaseSession extends Purchase {
+	
+	public interface Tokenizer extends Purchase.Tokenizer {
+		String previewModuleId();
+	}
+
+	/**
+	 * Preview module identifier (relevant only for subscription)
+	 */
+	private Integer previewModuleId;
+
+	// previewModuleId:
+	public Integer getPreviewModuleId(){
+		return this.previewModuleId;
+	}
+	public void setPreviewModuleId(Integer previewModuleId){
+		this.previewModuleId = previewModuleId;
+	}
+
+	public void previewModuleId(String multirequestToken){
+		setToken("previewModuleId", multirequestToken);
+	}
+
+
+	public PurchaseSession() {
+		super();
+	}
+
+	public PurchaseSession(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		previewModuleId = GsonParser.parseInt(jsonObject.get("previewModuleId"));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaPurchaseSession");
+		kparams.add("previewModuleId", this.previewModuleId);
+		return kparams;
+	}
+
+
+    public static final Creator<PurchaseSession> CREATOR = new Creator<PurchaseSession>() {
+        @Override
+        public PurchaseSession createFromParcel(Parcel source) {
+            return new PurchaseSession(source);
+        }
+
+        @Override
+        public PurchaseSession[] newArray(int size) {
+            return new PurchaseSession[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeValue(this.previewModuleId);
+    }
+
+    public PurchaseSession(Parcel in) {
+        super(in);
+        this.previewModuleId = (Integer)in.readValue(Integer.class.getClassLoader());
+    }
+}
+
