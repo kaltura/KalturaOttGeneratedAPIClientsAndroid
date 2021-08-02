2ad3d62958884f9abc2d7011a694985c2c3853e2
Backend CI <ott.rnd.core@kaltura.com>
2021-08-02 13:22:31 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/297, branch 6_6_1

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -44,8 +44,8 @@ public class Client extends ClientBase {
 	public Client(ConnectionConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:21-07-30");
-		this.setApiVersion("6.6.0.29206");
+		this.setClientTag("java:21-08-02");
+		this.setApiVersion("6.6.0.29207");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
