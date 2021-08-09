95234e9ab063c887bc9818a34e93ceb21208e5a9
Backend CI <ott.rnd.core@kaltura.com>
2021-08-09 17:18:29 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/304, branch 6_2_7

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/PricePlan.java
@@ -0,0 +1,154 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+/**
+ * Price plan
+ */
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(PricePlan.Tokenizer.class)
+public class PricePlan extends UsageModule {
+	
+	public interface Tokenizer extends UsageModule.Tokenizer {
+		String isRenewable();
+		String renewalsNumber();
+		String discountId();
+		String priceDetailsId();
+	}
+
+	/**
+	 * Denotes whether or not this object can be renewed
+	 */
+	private Boolean isRenewable;
+	/**
+	 * Defines the number of times the module will be renewed (for the life_cycle
+	  period)
+	 */
+	private Integer renewalsNumber;
+	/**
+	 * The discount module identifier of the price plan
+	 */
+	private Long discountId;
+	/**
+	 * The ID of the price details associated with this price plan
+	 */
+	private Long priceDetailsId;
+
+	// isRenewable:
+	public Boolean getIsRenewable(){
+		return this.isRenewable;
+	}
+	// renewalsNumber:
+	public Integer getRenewalsNumber(){
+		return this.renewalsNumber;
+	}
+	// discountId:
+	public Long getDiscountId(){
+		return this.discountId;
+	}
+	// priceDetailsId:
+	public Long getPriceDetailsId(){
+		return this.priceDetailsId;
+	}
+	public void setPriceDetailsId(Long priceDetailsId){
+		this.priceDetailsId = priceDetailsId;
+	}
+
+	public void priceDetailsId(String multirequestToken){
+		setToken("priceDetailsId", multirequestToken);
+	}
+
+
+	public PricePlan() {
+		super();
+	}
+
+	public PricePlan(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		isRenewable = GsonParser.parseBoolean(jsonObject.get("isRenewable"));
+		renewalsNumber = GsonParser.parseInt(jsonObject.get("renewalsNumber"));
+		discountId = GsonParser.parseLong(jsonObject.get("discountId"));
+		priceDetailsId = GsonParser.parseLong(jsonObject.get("priceDetailsId"));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaPricePlan");
+		kparams.add("priceDetailsId", this.priceDetailsId);
+		return kparams;
+	}
+
+
+    public static final Creator<PricePlan> CREATOR = new Creator<PricePlan>() {
+        @Override
+        public PricePlan createFromParcel(Parcel source) {
+            return new PricePlan(source);
+        }
+
+        @Override
+        public PricePlan[] newArray(int size) {
+            return new PricePlan[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeValue(this.isRenewable);
+        dest.writeValue(this.renewalsNumber);
+        dest.writeValue(this.discountId);
+        dest.writeValue(this.priceDetailsId);
+    }
+
+    public PricePlan(Parcel in) {
+        super(in);
+        this.isRenewable = (Boolean)in.readValue(Boolean.class.getClassLoader());
+        this.renewalsNumber = (Integer)in.readValue(Integer.class.getClassLoader());
+        this.discountId = (Long)in.readValue(Long.class.getClassLoader());
+        this.priceDetailsId = (Long)in.readValue(Long.class.getClassLoader());
+    }
+}
+
