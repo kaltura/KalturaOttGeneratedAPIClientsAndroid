6d1c8c5ad7e377860cb76c757ea787f924e5c499
Backend CI <ott.rnd.core@kaltura.com>
2022-09-12 20:14:38 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/513, branch 8_0_0_1

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/PersonalAssetSelectionFilter.java
@@ -0,0 +1,114 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(PersonalAssetSelectionFilter.Tokenizer.class)
+public class PersonalAssetSelectionFilter extends Filter {
+	
+	public interface Tokenizer extends Filter.Tokenizer {
+		String slotNumberEqual();
+	}
+
+	/**
+	 * selected assets for specific slot number
+	 */
+	private Integer slotNumberEqual;
+
+	// slotNumberEqual:
+	public Integer getSlotNumberEqual(){
+		return this.slotNumberEqual;
+	}
+	public void setSlotNumberEqual(Integer slotNumberEqual){
+		this.slotNumberEqual = slotNumberEqual;
+	}
+
+	public void slotNumberEqual(String multirequestToken){
+		setToken("slotNumberEqual", multirequestToken);
+	}
+
+
+	public PersonalAssetSelectionFilter() {
+		super();
+	}
+
+	public PersonalAssetSelectionFilter(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		slotNumberEqual = GsonParser.parseInt(jsonObject.get("slotNumberEqual"));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaPersonalAssetSelectionFilter");
+		kparams.add("slotNumberEqual", this.slotNumberEqual);
+		return kparams;
+	}
+
+
+    public static final Creator<PersonalAssetSelectionFilter> CREATOR = new Creator<PersonalAssetSelectionFilter>() {
+        @Override
+        public PersonalAssetSelectionFilter createFromParcel(Parcel source) {
+            return new PersonalAssetSelectionFilter(source);
+        }
+
+        @Override
+        public PersonalAssetSelectionFilter[] newArray(int size) {
+            return new PersonalAssetSelectionFilter[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeValue(this.slotNumberEqual);
+    }
+
+    public PersonalAssetSelectionFilter(Parcel in) {
+        super(in);
+        this.slotNumberEqual = (Integer)in.readValue(Integer.class.getClassLoader());
+    }
+}
+
