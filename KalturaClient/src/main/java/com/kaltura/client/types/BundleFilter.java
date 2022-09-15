f8c87c3cb649e4500b79acc3a095059b1d7c2fc7
Backend CI <ott.rnd.core@kaltura.com>
2022-09-15 08:12:12 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/515, branch 8_0_1

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/BundleFilter.java
@@ -0,0 +1,161 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.enums.BundleType;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(BundleFilter.Tokenizer.class)
+public class BundleFilter extends AssetFilter {
+	
+	public interface Tokenizer extends AssetFilter.Tokenizer {
+		String idEqual();
+		String typeIn();
+		String bundleTypeEqual();
+	}
+
+	/**
+	 * Bundle Id.
+	 */
+	private Integer idEqual;
+	/**
+	 * Comma separated list of asset types to search within.               Possible
+	  values: 0 – EPG linear programs entries, any media type ID (according to media
+	  type IDs defined dynamically in the system).              If omitted – all
+	  types should be included.
+	 */
+	private String typeIn;
+	/**
+	 * bundleType - possible values: Subscription or Collection
+	 */
+	private BundleType bundleTypeEqual;
+
+	// idEqual:
+	public Integer getIdEqual(){
+		return this.idEqual;
+	}
+	public void setIdEqual(Integer idEqual){
+		this.idEqual = idEqual;
+	}
+
+	public void idEqual(String multirequestToken){
+		setToken("idEqual", multirequestToken);
+	}
+
+	// typeIn:
+	public String getTypeIn(){
+		return this.typeIn;
+	}
+	public void setTypeIn(String typeIn){
+		this.typeIn = typeIn;
+	}
+
+	public void typeIn(String multirequestToken){
+		setToken("typeIn", multirequestToken);
+	}
+
+	// bundleTypeEqual:
+	public BundleType getBundleTypeEqual(){
+		return this.bundleTypeEqual;
+	}
+	public void setBundleTypeEqual(BundleType bundleTypeEqual){
+		this.bundleTypeEqual = bundleTypeEqual;
+	}
+
+	public void bundleTypeEqual(String multirequestToken){
+		setToken("bundleTypeEqual", multirequestToken);
+	}
+
+
+	public BundleFilter() {
+		super();
+	}
+
+	public BundleFilter(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		idEqual = GsonParser.parseInt(jsonObject.get("idEqual"));
+		typeIn = GsonParser.parseString(jsonObject.get("typeIn"));
+		bundleTypeEqual = BundleType.get(GsonParser.parseString(jsonObject.get("bundleTypeEqual")));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaBundleFilter");
+		kparams.add("idEqual", this.idEqual);
+		kparams.add("typeIn", this.typeIn);
+		kparams.add("bundleTypeEqual", this.bundleTypeEqual);
+		return kparams;
+	}
+
+
+    public static final Creator<BundleFilter> CREATOR = new Creator<BundleFilter>() {
+        @Override
+        public BundleFilter createFromParcel(Parcel source) {
+            return new BundleFilter(source);
+        }
+
+        @Override
+        public BundleFilter[] newArray(int size) {
+            return new BundleFilter[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeValue(this.idEqual);
+        dest.writeString(this.typeIn);
+        dest.writeInt(this.bundleTypeEqual == null ? -1 : this.bundleTypeEqual.ordinal());
+    }
+
+    public BundleFilter(Parcel in) {
+        super(in);
+        this.idEqual = (Integer)in.readValue(Integer.class.getClassLoader());
+        this.typeIn = in.readString();
+        int tmpBundleTypeEqual = in.readInt();
+        this.bundleTypeEqual = tmpBundleTypeEqual == -1 ? null : BundleType.values()[tmpBundleTypeEqual];
+    }
+}
+
