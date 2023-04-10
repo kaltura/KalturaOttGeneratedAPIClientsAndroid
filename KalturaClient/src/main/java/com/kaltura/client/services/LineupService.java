0355e011b898b1e49f0e1c880d20241779cd5a5c
Backend CI <ott.rnd.core@kaltura.com>
2023-04-10 10:41:50 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/559, branch 8_7_0

---
+++ KalturaClient/src/main/java/com/kaltura/client/services/LineupService.java
@@ -0,0 +1,120 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.services;
+
+import com.kaltura.client.types.FilterPager;
+import com.kaltura.client.types.LineupChannelAsset;
+import com.kaltura.client.types.LineupRegionalChannelFilter;
+import com.kaltura.client.utils.request.ListResponseRequestBuilder;
+import com.kaltura.client.utils.request.RequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+public class LineupService {
+	
+	public static class GetLineupBuilder extends ListResponseRequestBuilder<LineupChannelAsset, LineupChannelAsset.Tokenizer, GetLineupBuilder> {
+		
+		public GetLineupBuilder(int pageIndex, int pageSize) {
+			super(LineupChannelAsset.class, "lineup", "get");
+			params.add("pageIndex", pageIndex);
+			params.add("pageSize", pageSize);
+		}
+		
+		public void pageIndex(String multirequestToken) {
+			params.add("pageIndex", multirequestToken);
+		}
+		
+		public void pageSize(String multirequestToken) {
+			params.add("pageSize", multirequestToken);
+		}
+	}
+
+	/**
+	 * Return regional lineup (list of lineup channel asset objects) based on the
+	  requester session characteristics and his region.
+	 * 
+	 * @param pageIndex Page index - The page index to retrieve, (if it is not sent the default page
+	 * size is 1).
+	 * @param pageSize Page size - The page size to retrieve. Must be one of the follow numbers: 100,
+	 * 200, 800, 1200, 1600 (if it is not sent the default page size is 500).
+	 */
+    public static GetLineupBuilder get(int pageIndex, int pageSize)  {
+		return new GetLineupBuilder(pageIndex, pageSize);
+	}
+	
+	public static class ListLineupBuilder extends ListResponseRequestBuilder<LineupChannelAsset, LineupChannelAsset.Tokenizer, ListLineupBuilder> {
+		
+		public ListLineupBuilder(LineupRegionalChannelFilter filter, FilterPager pager) {
+			super(LineupChannelAsset.class, "lineup", "list");
+			params.add("filter", filter);
+			params.add("pager", pager);
+		}
+	}
+
+	public static ListLineupBuilder list(LineupRegionalChannelFilter filter)  {
+		return list(filter, null);
+	}
+
+	/**
+	 * Returns list of lineup regional linear channels associated with one LCN and its
+	  region information. Allows to apply sorting and filtering by LCN and linear
+	  channels.
+	 * 
+	 * @param filter Request filter
+	 * @param pager Paging the request
+	 */
+    public static ListLineupBuilder list(LineupRegionalChannelFilter filter, FilterPager pager)  {
+		return new ListLineupBuilder(filter, pager);
+	}
+	
+	public static class SendUpdatedNotificationLineupBuilder extends RequestBuilder<Boolean, String, SendUpdatedNotificationLineupBuilder> {
+		
+		public SendUpdatedNotificationLineupBuilder(String regionIds) {
+			super(Boolean.class, "lineup", "sendUpdatedNotification");
+			params.add("regionIds", regionIds);
+		}
+		
+		public void regionIds(String multirequestToken) {
+			params.add("regionIds", multirequestToken);
+		}
+	}
+
+	/**
+	 * Sends lineup update requested notification.
+	 * 
+	 * @param regionIds Region IDs separated by commas.
+	 */
+    public static SendUpdatedNotificationLineupBuilder sendUpdatedNotification(String regionIds)  {
+		return new SendUpdatedNotificationLineupBuilder(regionIds);
+	}
+}
---
+++ KalturaClient/src/main/java/com/kaltura/client/types/DateTrigger.java
@@ -0,0 +1,114 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(DateTrigger.Tokenizer.class)
+public class DateTrigger extends Trigger {
+	
+	public interface Tokenizer extends Trigger.Tokenizer {
+		String date();
+	}
+
+	/**
+	 * Trigger date
+	 */
+	private Long date;
+
+	// date:
+	public Long getDate(){
+		return this.date;
+	}
+	public void setDate(Long date){
+		this.date = date;
+	}
+
+	public void date(String multirequestToken){
+		setToken("date", multirequestToken);
+	}
+
+
+	public DateTrigger() {
+		super();
+	}
+
+	public DateTrigger(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		date = GsonParser.parseLong(jsonObject.get("date"));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaDateTrigger");
+		kparams.add("date", this.date);
+		return kparams;
+	}
+
+
+    public static final Creator<DateTrigger> CREATOR = new Creator<DateTrigger>() {
+        @Override
+        public DateTrigger createFromParcel(Parcel source) {
+            return new DateTrigger(source);
+        }
+
+        @Override
+        public DateTrigger[] newArray(int size) {
+            return new DateTrigger[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeValue(this.date);
+    }
+
+    public DateTrigger(Parcel in) {
+        super(in);
+        this.date = (Long)in.readValue(Long.class.getClassLoader());
+    }
+}
+
