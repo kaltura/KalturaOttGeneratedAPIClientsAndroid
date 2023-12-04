641dc6ab9cda4d6de03581a4255861a5f390eb19
Backend CI <ott.rnd.core@kaltura.com>
2023-12-04 11:23:17 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/628, branch 9.5.0.1

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/ChannelDynamicOrder.java
@@ -0,0 +1,137 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.enums.MetaTagOrderBy;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(ChannelDynamicOrder.Tokenizer.class)
+public class ChannelDynamicOrder extends BaseChannelOrder {
+	
+	public interface Tokenizer extends BaseChannelOrder.Tokenizer {
+		String name();
+		String orderBy();
+	}
+
+	/**
+	 * Value
+	 */
+	private String name;
+	/**
+	 * Order By
+	 */
+	private MetaTagOrderBy orderBy;
+
+	// name:
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public void name(String multirequestToken){
+		setToken("name", multirequestToken);
+	}
+
+	// orderBy:
+	public MetaTagOrderBy getOrderBy(){
+		return this.orderBy;
+	}
+	public void setOrderBy(MetaTagOrderBy orderBy){
+		this.orderBy = orderBy;
+	}
+
+	public void orderBy(String multirequestToken){
+		setToken("orderBy", multirequestToken);
+	}
+
+
+	public ChannelDynamicOrder() {
+		super();
+	}
+
+	public ChannelDynamicOrder(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		name = GsonParser.parseString(jsonObject.get("name"));
+		orderBy = MetaTagOrderBy.get(GsonParser.parseString(jsonObject.get("orderBy")));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaChannelDynamicOrder");
+		kparams.add("name", this.name);
+		kparams.add("orderBy", this.orderBy);
+		return kparams;
+	}
+
+
+    public static final Creator<ChannelDynamicOrder> CREATOR = new Creator<ChannelDynamicOrder>() {
+        @Override
+        public ChannelDynamicOrder createFromParcel(Parcel source) {
+            return new ChannelDynamicOrder(source);
+        }
+
+        @Override
+        public ChannelDynamicOrder[] newArray(int size) {
+            return new ChannelDynamicOrder[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(this.name);
+        dest.writeInt(this.orderBy == null ? -1 : this.orderBy.ordinal());
+    }
+
+    public ChannelDynamicOrder(Parcel in) {
+        super(in);
+        this.name = in.readString();
+        int tmpOrderBy = in.readInt();
+        this.orderBy = tmpOrderBy == -1 ? null : MetaTagOrderBy.values()[tmpOrderBy];
+    }
+}
+
