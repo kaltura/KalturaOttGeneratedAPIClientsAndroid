352212b6341e28bffd57b296e7721250dd6e1b12
Backend CI <ott.rnd.core@kaltura.com>
2024-05-24 15:53:15 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/662, branch 10.0.0.3

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/BillingPartnerConfig.java
@@ -0,0 +1,140 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.enums.PartnerConfigurationType;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+/**
+ * Partner billing configuration
+ */
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(BillingPartnerConfig.Tokenizer.class)
+public class BillingPartnerConfig extends PartnerConfiguration {
+	
+	public interface Tokenizer extends PartnerConfiguration.Tokenizer {
+		String value();
+		String type();
+	}
+
+	/**
+	 * configuration value
+	 */
+	private String value;
+	/**
+	 * partner configuration type
+	 */
+	private PartnerConfigurationType type;
+
+	// value:
+	public String getValue(){
+		return this.value;
+	}
+	public void setValue(String value){
+		this.value = value;
+	}
+
+	public void value(String multirequestToken){
+		setToken("value", multirequestToken);
+	}
+
+	// type:
+	public PartnerConfigurationType getType(){
+		return this.type;
+	}
+	public void setType(PartnerConfigurationType type){
+		this.type = type;
+	}
+
+	public void type(String multirequestToken){
+		setToken("type", multirequestToken);
+	}
+
+
+	public BillingPartnerConfig() {
+		super();
+	}
+
+	public BillingPartnerConfig(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		value = GsonParser.parseString(jsonObject.get("value"));
+		type = PartnerConfigurationType.get(GsonParser.parseString(jsonObject.get("type")));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaBillingPartnerConfig");
+		kparams.add("value", this.value);
+		kparams.add("type", this.type);
+		return kparams;
+	}
+
+
+    public static final Creator<BillingPartnerConfig> CREATOR = new Creator<BillingPartnerConfig>() {
+        @Override
+        public BillingPartnerConfig createFromParcel(Parcel source) {
+            return new BillingPartnerConfig(source);
+        }
+
+        @Override
+        public BillingPartnerConfig[] newArray(int size) {
+            return new BillingPartnerConfig[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(this.value);
+        dest.writeInt(this.type == null ? -1 : this.type.ordinal());
+    }
+
+    public BillingPartnerConfig(Parcel in) {
+        super(in);
+        this.value = in.readString();
+        int tmpType = in.readInt();
+        this.type = tmpType == -1 ? null : PartnerConfigurationType.values()[tmpType];
+    }
+}
+
---
+++ KalturaClient/src/main/java/com/kaltura/client/types/HouseholdSegmentFilter.java
@@ -0,0 +1,114 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(HouseholdSegmentFilter.Tokenizer.class)
+public class HouseholdSegmentFilter extends Filter {
+	
+	public interface Tokenizer extends Filter.Tokenizer {
+		String kSql();
+	}
+
+	/**
+	 * KSQL expression
+	 */
+	private String kSql;
+
+	// kSql:
+	public String getKSql(){
+		return this.kSql;
+	}
+	public void setKSql(String kSql){
+		this.kSql = kSql;
+	}
+
+	public void kSql(String multirequestToken){
+		setToken("kSql", multirequestToken);
+	}
+
+
+	public HouseholdSegmentFilter() {
+		super();
+	}
+
+	public HouseholdSegmentFilter(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		kSql = GsonParser.parseString(jsonObject.get("kSql"));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaHouseholdSegmentFilter");
+		kparams.add("kSql", this.kSql);
+		return kparams;
+	}
+
+
+    public static final Creator<HouseholdSegmentFilter> CREATOR = new Creator<HouseholdSegmentFilter>() {
+        @Override
+        public HouseholdSegmentFilter createFromParcel(Parcel source) {
+            return new HouseholdSegmentFilter(source);
+        }
+
+        @Override
+        public HouseholdSegmentFilter[] newArray(int size) {
+            return new HouseholdSegmentFilter[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(this.kSql);
+    }
+
+    public HouseholdSegmentFilter(Parcel in) {
+        super(in);
+        this.kSql = in.readString();
+    }
+}
+
