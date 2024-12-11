5c90734118801fc841a350bbb7f6a14ee3cc28e1
Backend CI <ott.rnd.core@kaltura.com>
2024-12-11 10:49:01 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/708, branch 10.7.1.0

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/PurchaseSettings.java
@@ -0,0 +1,119 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.enums.PurchaseSettingsType;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+/**
+ * Purchase settings and PIN
+ */
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(PurchaseSettings.Tokenizer.class)
+public class PurchaseSettings extends Pin {
+	
+	public interface Tokenizer extends Pin.Tokenizer {
+		String permission();
+	}
+
+	/**
+	 * Purchase permission - block, ask or allow
+	 */
+	private PurchaseSettingsType permission;
+
+	// permission:
+	public PurchaseSettingsType getPermission(){
+		return this.permission;
+	}
+	public void setPermission(PurchaseSettingsType permission){
+		this.permission = permission;
+	}
+
+	public void permission(String multirequestToken){
+		setToken("permission", multirequestToken);
+	}
+
+
+	public PurchaseSettings() {
+		super();
+	}
+
+	public PurchaseSettings(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		permission = PurchaseSettingsType.get(GsonParser.parseString(jsonObject.get("permission")));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaPurchaseSettings");
+		kparams.add("permission", this.permission);
+		return kparams;
+	}
+
+
+    public static final Creator<PurchaseSettings> CREATOR = new Creator<PurchaseSettings>() {
+        @Override
+        public PurchaseSettings createFromParcel(Parcel source) {
+            return new PurchaseSettings(source);
+        }
+
+        @Override
+        public PurchaseSettings[] newArray(int size) {
+            return new PurchaseSettings[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeInt(this.permission == null ? -1 : this.permission.ordinal());
+    }
+
+    public PurchaseSettings(Parcel in) {
+        super(in);
+        int tmpPermission = in.readInt();
+        this.permission = tmpPermission == -1 ? null : PurchaseSettingsType.values()[tmpPermission];
+    }
+}
+
