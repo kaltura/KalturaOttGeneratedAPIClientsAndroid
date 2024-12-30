f525c91e18356e56e060328bf6bbde2f5d0424f9
Backend CI <ott.rnd.core@kaltura.com>
2024-12-30 19:11:23 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/714, branch 10.7.0.5

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/MonetizationCondition.java
@@ -0,0 +1,248 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.enums.MathemticalOperatorType;
+import com.kaltura.client.enums.MonetizationType;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+/**
+ * Defines a condition which is essentially a combination of several
+  monetization-based actions, each has their own score multiplier
+ */
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(MonetizationCondition.Tokenizer.class)
+public class MonetizationCondition extends BaseSegmentCondition {
+	
+	public interface Tokenizer extends BaseSegmentCondition.Tokenizer {
+		String days();
+		String type();
+		String operator();
+		String businessModuleIdIn();
+		String currencyCode();
+		String minValue();
+		String maxValue();
+	}
+
+	/**
+	 * How many days back should the actions be considered
+	 */
+	private Integer days;
+	/**
+	 * Purchase type
+	 */
+	private MonetizationType type;
+	/**
+	 * Mathermtical operator to calculate
+	 */
+	private MathemticalOperatorType operator;
+	/**
+	 * Comma saperated list of business module IDs
+	 */
+	private String businessModuleIdIn;
+	/**
+	 * Which currency code should be taken into consideration
+	 */
+	private String currencyCode;
+	/**
+	 * The minimum value to be met
+	 */
+	private Integer minValue;
+	/**
+	 * The maximum value to be met
+	 */
+	private Integer maxValue;
+
+	// days:
+	public Integer getDays(){
+		return this.days;
+	}
+	public void setDays(Integer days){
+		this.days = days;
+	}
+
+	public void days(String multirequestToken){
+		setToken("days", multirequestToken);
+	}
+
+	// type:
+	public MonetizationType getType(){
+		return this.type;
+	}
+	public void setType(MonetizationType type){
+		this.type = type;
+	}
+
+	public void type(String multirequestToken){
+		setToken("type", multirequestToken);
+	}
+
+	// operator:
+	public MathemticalOperatorType getOperator(){
+		return this.operator;
+	}
+	public void setOperator(MathemticalOperatorType operator){
+		this.operator = operator;
+	}
+
+	public void operator(String multirequestToken){
+		setToken("operator", multirequestToken);
+	}
+
+	// businessModuleIdIn:
+	public String getBusinessModuleIdIn(){
+		return this.businessModuleIdIn;
+	}
+	public void setBusinessModuleIdIn(String businessModuleIdIn){
+		this.businessModuleIdIn = businessModuleIdIn;
+	}
+
+	public void businessModuleIdIn(String multirequestToken){
+		setToken("businessModuleIdIn", multirequestToken);
+	}
+
+	// currencyCode:
+	public String getCurrencyCode(){
+		return this.currencyCode;
+	}
+	public void setCurrencyCode(String currencyCode){
+		this.currencyCode = currencyCode;
+	}
+
+	public void currencyCode(String multirequestToken){
+		setToken("currencyCode", multirequestToken);
+	}
+
+	// minValue:
+	public Integer getMinValue(){
+		return this.minValue;
+	}
+	public void setMinValue(Integer minValue){
+		this.minValue = minValue;
+	}
+
+	public void minValue(String multirequestToken){
+		setToken("minValue", multirequestToken);
+	}
+
+	// maxValue:
+	public Integer getMaxValue(){
+		return this.maxValue;
+	}
+	public void setMaxValue(Integer maxValue){
+		this.maxValue = maxValue;
+	}
+
+	public void maxValue(String multirequestToken){
+		setToken("maxValue", multirequestToken);
+	}
+
+
+	public MonetizationCondition() {
+		super();
+	}
+
+	public MonetizationCondition(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		days = GsonParser.parseInt(jsonObject.get("days"));
+		type = MonetizationType.get(GsonParser.parseString(jsonObject.get("type")));
+		operator = MathemticalOperatorType.get(GsonParser.parseString(jsonObject.get("operator")));
+		businessModuleIdIn = GsonParser.parseString(jsonObject.get("businessModuleIdIn"));
+		currencyCode = GsonParser.parseString(jsonObject.get("currencyCode"));
+		minValue = GsonParser.parseInt(jsonObject.get("minValue"));
+		maxValue = GsonParser.parseInt(jsonObject.get("maxValue"));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaMonetizationCondition");
+		kparams.add("days", this.days);
+		kparams.add("type", this.type);
+		kparams.add("operator", this.operator);
+		kparams.add("businessModuleIdIn", this.businessModuleIdIn);
+		kparams.add("currencyCode", this.currencyCode);
+		kparams.add("minValue", this.minValue);
+		kparams.add("maxValue", this.maxValue);
+		return kparams;
+	}
+
+
+    public static final Creator<MonetizationCondition> CREATOR = new Creator<MonetizationCondition>() {
+        @Override
+        public MonetizationCondition createFromParcel(Parcel source) {
+            return new MonetizationCondition(source);
+        }
+
+        @Override
+        public MonetizationCondition[] newArray(int size) {
+            return new MonetizationCondition[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeValue(this.days);
+        dest.writeInt(this.type == null ? -1 : this.type.ordinal());
+        dest.writeInt(this.operator == null ? -1 : this.operator.ordinal());
+        dest.writeString(this.businessModuleIdIn);
+        dest.writeString(this.currencyCode);
+        dest.writeValue(this.minValue);
+        dest.writeValue(this.maxValue);
+    }
+
+    public MonetizationCondition(Parcel in) {
+        super(in);
+        this.days = (Integer)in.readValue(Integer.class.getClassLoader());
+        int tmpType = in.readInt();
+        this.type = tmpType == -1 ? null : MonetizationType.values()[tmpType];
+        int tmpOperator = in.readInt();
+        this.operator = tmpOperator == -1 ? null : MathemticalOperatorType.values()[tmpOperator];
+        this.businessModuleIdIn = in.readString();
+        this.currencyCode = in.readString();
+        this.minValue = (Integer)in.readValue(Integer.class.getClassLoader());
+        this.maxValue = (Integer)in.readValue(Integer.class.getClassLoader());
+    }
+}
+
---
+++ KalturaClient/src/main/java/com/kaltura/client/types/PartnerPremiumServices.java
@@ -0,0 +1,123 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.types.ObjectBase;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+import com.kaltura.client.utils.request.RequestBuilder;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(PartnerPremiumServices.Tokenizer.class)
+public class PartnerPremiumServices extends ObjectBase {
+	
+	public interface Tokenizer extends ObjectBase.Tokenizer {
+		RequestBuilder.ListTokenizer<PartnerPremiumService.Tokenizer> objects();
+	}
+
+	/**
+	 * A list of services
+	 */
+	private List<PartnerPremiumService> objects;
+
+	// objects:
+	public List<PartnerPremiumService> getObjects(){
+		return this.objects;
+	}
+	public void setObjects(List<PartnerPremiumService> objects){
+		this.objects = objects;
+	}
+
+
+	public PartnerPremiumServices() {
+		super();
+	}
+
+	public PartnerPremiumServices(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		objects = GsonParser.parseArray(jsonObject.getAsJsonArray("objects"), PartnerPremiumService.class);
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaPartnerPremiumServices");
+		kparams.add("objects", this.objects);
+		return kparams;
+	}
+
+
+    public static final Creator<PartnerPremiumServices> CREATOR = new Creator<PartnerPremiumServices>() {
+        @Override
+        public PartnerPremiumServices createFromParcel(Parcel source) {
+            return new PartnerPremiumServices(source);
+        }
+
+        @Override
+        public PartnerPremiumServices[] newArray(int size) {
+            return new PartnerPremiumServices[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        if(this.objects != null) {
+            dest.writeInt(this.objects.size());
+            dest.writeList(this.objects);
+        } else {
+            dest.writeInt(-1);
+        }
+    }
+
+    public PartnerPremiumServices(Parcel in) {
+        super(in);
+        int objectsSize = in.readInt();
+        if( objectsSize > -1) {
+            this.objects = new ArrayList<>();
+            in.readList(this.objects, PartnerPremiumService.class.getClassLoader());
+        }
+    }
+}
+
