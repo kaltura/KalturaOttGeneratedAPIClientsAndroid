c5f453decff4e41f4620274b2561be057bfef10a
Backend CI <ott.rnd.core@kaltura.com>
2025-02-27 13:23:37 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/724, branch 10.6.0.10

---
+++ KalturaClient/src/main/java/com/kaltura/client/types/EngagementAdapterBase.java
@@ -0,0 +1,130 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2020  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+package com.kaltura.client.types;
+
+import android.os.Parcel;
+import com.google.gson.JsonObject;
+import com.kaltura.client.Params;
+import com.kaltura.client.types.ObjectBase;
+import com.kaltura.client.utils.GsonParser;
+import com.kaltura.client.utils.request.MultiRequestBuilder;
+
+/**
+ * This class was generated using exec.php
+ * against an XML schema provided by Kaltura.
+ * 
+ * MANUAL CHANGES TO THIS CLASS WILL BE OVERWRITTEN.
+ */
+
+/**
+ * Engagement adapter basic
+ */
+@SuppressWarnings("serial")
+@MultiRequestBuilder.Tokenizer(EngagementAdapterBase.Tokenizer.class)
+public class EngagementAdapterBase extends ObjectBase {
+	
+	public interface Tokenizer extends ObjectBase.Tokenizer {
+		String id();
+		String name();
+	}
+
+	/**
+	 * Engagement adapter id
+	 */
+	private Integer id;
+	/**
+	 * Engagement adapter name
+	 */
+	private String name;
+
+	// id:
+	public Integer getId(){
+		return this.id;
+	}
+	// name:
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public void name(String multirequestToken){
+		setToken("name", multirequestToken);
+	}
+
+
+	public EngagementAdapterBase() {
+		super();
+	}
+
+	public EngagementAdapterBase(JsonObject jsonObject) throws APIException {
+		super(jsonObject);
+
+		if(jsonObject == null) return;
+
+		// set members values:
+		id = GsonParser.parseInt(jsonObject.get("id"));
+		name = GsonParser.parseString(jsonObject.get("name"));
+
+	}
+
+	public Params toParams() {
+		Params kparams = super.toParams();
+		kparams.add("objectType", "KalturaEngagementAdapterBase");
+		kparams.add("name", this.name);
+		return kparams;
+	}
+
+
+    public static final Creator<EngagementAdapterBase> CREATOR = new Creator<EngagementAdapterBase>() {
+        @Override
+        public EngagementAdapterBase createFromParcel(Parcel source) {
+            return new EngagementAdapterBase(source);
+        }
+
+        @Override
+        public EngagementAdapterBase[] newArray(int size) {
+            return new EngagementAdapterBase[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeValue(this.id);
+        dest.writeString(this.name);
+    }
+
+    public EngagementAdapterBase(Parcel in) {
+        super(in);
+        this.id = (Integer)in.readValue(Integer.class.getClassLoader());
+        this.name = in.readString();
+    }
+}
+
