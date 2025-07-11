2e4032a905c1f57ef94494f865807fb42570dbfe
Backend CI <ott.rnd.core@kaltura.com>
2025-07-11 09:43:37 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/761, branch 11.4.0.3

--- KalturaClient/src/main/java/com/kaltura/client/types/RecordingFilter.java
+++ KalturaClient/src/main/java/com/kaltura/client/types/RecordingFilter.java
@@ -49,6 +49,7 @@ public class RecordingFilter extends Filter {
 	
 	public interface Tokenizer extends Filter.Tokenizer {
 		String statusIn();
+		String assetIdIn();
 		String externalRecordingIdIn();
 		String kSql();
 	}
@@ -57,6 +58,10 @@ public class RecordingFilter extends Filter {
 	 * Recording Statuses
 	 */
 	private String statusIn;
+	/**
+	 * Comma separated list of assets identifiers
+	 */
+	private String assetIdIn;
 	/**
 	 * Comma separated external identifiers
 	 */
@@ -76,6 +81,18 @@ public class RecordingFilter extends Filter {
 
 	public void statusIn(String multirequestToken){
 		setToken("statusIn", multirequestToken);
+	}
+
+	// assetIdIn:
+	public String getAssetIdIn(){
+		return this.assetIdIn;
+	}
+	public void setAssetIdIn(String assetIdIn){
+		this.assetIdIn = assetIdIn;
+	}
+
+	public void assetIdIn(String multirequestToken){
+		setToken("assetIdIn", multirequestToken);
 	}
 
 	// externalRecordingIdIn:
@@ -114,6 +131,7 @@ public class RecordingFilter extends Filter {
 
 		// set members values:
 		statusIn = GsonParser.parseString(jsonObject.get("statusIn"));
+		assetIdIn = GsonParser.parseString(jsonObject.get("assetIdIn"));
 		externalRecordingIdIn = GsonParser.parseString(jsonObject.get("externalRecordingIdIn"));
 		kSql = GsonParser.parseString(jsonObject.get("kSql"));
 
@@ -123,6 +141,7 @@ public class RecordingFilter extends Filter {
 		Params kparams = super.toParams();
 		kparams.add("objectType", "KalturaRecordingFilter");
 		kparams.add("statusIn", this.statusIn);
+		kparams.add("assetIdIn", this.assetIdIn);
 		kparams.add("externalRecordingIdIn", this.externalRecordingIdIn);
 		kparams.add("kSql", this.kSql);
 		return kparams;
@@ -145,6 +164,7 @@ public class RecordingFilter extends Filter {
     public void writeToParcel(Parcel dest, int flags) {
         super.writeToParcel(dest, flags);
         dest.writeString(this.statusIn);
+        dest.writeString(this.assetIdIn);
         dest.writeString(this.externalRecordingIdIn);
         dest.writeString(this.kSql);
     }
@@ -152,6 +172,7 @@ public class RecordingFilter extends Filter {
     public RecordingFilter(Parcel in) {
         super(in);
         this.statusIn = in.readString();
+        this.assetIdIn = in.readString();
         this.externalRecordingIdIn = in.readString();
         this.kSql = in.readString();
     }
