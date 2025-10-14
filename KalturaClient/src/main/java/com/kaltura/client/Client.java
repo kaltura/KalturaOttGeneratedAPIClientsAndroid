7b8f3239d83d3d34998869738bf7e71294b01472
Backend CI <ott.rnd.core@kaltura.com>
2025-10-14 09:12:41 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs-ios-and-android/779, branch 11.7.0.1

--- KalturaClient/src/main/java/com/kaltura/client/Client.java
+++ KalturaClient/src/main/java/com/kaltura/client/Client.java
@@ -44,8 +44,8 @@ public class Client extends ClientBase {
 	public Client(ConnectionConfiguration config) {
 		super(config);
 		
-		this.setClientTag("java:25-04-14");
-		this.setApiVersion("11.1.0.1");
+		this.setClientTag("java:25-10-14");
+		this.setApiVersion("11.7.0.1");
 		this.clientConfiguration.put("format", 1); // JSON
 	}
 	
--- KalturaClient/src/main/java/com/kaltura/client/services/SubtitlesService.java
+++ KalturaClient/src/main/java/com/kaltura/client/services/SubtitlesService.java
@@ -68,10 +68,12 @@ public class SubtitlesService {
 	}
 
 	/**
-	 * Upload a subtitles file for a later analysis.
+	 * Add a subtitles file to be used for generating metadata and enriching the assets
+	  using a multi-part form-data body including the JSON configuration object and
+	  the uploaded file.
 	 * 
-	 * @param subtitles Subtitle metadata
-	 * @param fileData The subtitles text file to upload. Must be in UTF-8 encoding.
+	 * @param subtitles Subtitle file metadata.
+	 * @param fileData The subtitles file to upload. The file must be in UTF-8 encoding.
 	 */
     public static UploadFileSubtitlesBuilder uploadFile(UploadSubtitles subtitles, FileHolder fileData)  {
 		return new UploadFileSubtitlesBuilder(subtitles, fileData);
